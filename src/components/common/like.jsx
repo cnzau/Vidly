462b5581d8b868b9a9f04b6155433e2f12253afc
Clement Nzau <clement.nzau@gmail.com>
2018-10-16 17:26:31 +0300

    Change Like class component to stateless functional component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,23 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Input: liked: boolean
 // Output: onClick
 // Raises an onClick event then renders empty or full hearts
 // not related to movies
+// This is a controlled component. Receives all data and notify changes via props
+// We only have render method hence can convert to sfc
+// pass props and delete 'this.'
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        className={classes}
-        aria-hidden="true"
-      />
-    );
-  }
-}
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
 
 export default Like;
